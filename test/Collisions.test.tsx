f4ca306d33df8e0d5c49e4812042daec99bd87c8
juandeaglio <casualgamer105@yahoo.com>
2024-06-06 15:05:01 -0400

    Fixed test coupled to element's transform (poor test design)

--- test/Collisions.test.tsx
+++ test/Collisions.test.tsx
@@ -4,29 +4,28 @@ import '@testing-library/jest-dom'
 import { ReactNode, useRef } from 'react';
 import { render } from '@testing-library/react';
 import { AnimatedRect} from '../src/Components/AnimatedRect';
-import { screen } from '@testing-library/dom';
 import { Collisions } from '../src/Collisions';
 import { RenderableElement } from '../src/RenderableElement';
 import { createdMockedGetBoundingClientRect } from '../src/createdMockedGetBoundingClientRect';
 import { ViewportBarriers, generateViewport} from '../src/ViewportBarriers';
 import { ScreenLimitations } from './ScreenLimitations';
-import { parseTransform } from './ParseTransform';
 import { Vector } from '../src/Components/Vector';
 
 interface AppProps {
     children?: ReactNode;
 }
 
+const screenWidth = new ScreenLimitations().screenWidth;
+const rectWidth = new ScreenLimitations().rectWidth;
+const vector = screenWidth - rectWidth + 1;
 function App({children}: AppProps)
 {
     const rect1 = useRef<SVGSVGElement>(null); // I predict type is going to change, consider abstract
 
-    const screenWidth = new ScreenLimitations().screenWidth;
-    const rectWidth = new ScreenLimitations().rectWidth;
     return(
         <div>
             <div>
-                <AnimatedRect moreProps={{"data-testid": "Box-1"}} ref={rect1} velocityVector={new Vector(screenWidth - rectWidth + 1,0)}/>
+                <AnimatedRect moreProps={{"data-testid": "Box-1"}} ref={rect1} velocityVector={new Vector(vector,0)}/>
                 {children}
             </div>
         </div>
@@ -44,9 +43,7 @@ describe('collisionCalculator class', () => {
 
     test('We check for a collision with the right edge of screen', () => {
         render(<App></App>);
-        const element = screen.getByTestId("Box-1").style.transform;
-        const xActual = parseTransform(element)[0];
-        const box1Rect: RenderableElement = new RenderableElement(createdMockedGetBoundingClientRect(xActual, 0, rectWidth, rectWidth));
+        const box1Rect: RenderableElement = new RenderableElement(createdMockedGetBoundingClientRect(vector, 0, rectWidth, rectWidth));
         const viewPortRect: RenderableElement = viewPortBarriers.right;
         const collisionCalculator = new Collisions();
     
